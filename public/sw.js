55d5a041898257456d6f472fe4ef9f041e2004bf
Muhammad Masham <66207787+masham04@users.noreply.github.com>
2021-03-22 16:37:16 +0500

    new

---
+++ public/sw.js
@@ -0,0 +1,40 @@
+
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/sw.js',
+  'index.html',
+  'index.js',
+  'App.js',
+  'App.css',
+  '/static/js/0.chunk.js',
+  '/static/js/main.chunk.js',
+  '/static/js/bundle.js',
+  
+
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// self.addEventListener('fetch', function(event) {
+//     event.respondWith(
+//       caches.match(event.request)
+//         .then(function(response) {
+//           // Cache hit - return response
+//           if (response) {
+//             return response;
+//           }
+//           return fetch(event.request);
+//         }
+//       )
+//     );
+//   });
--- src/components/Layout/App.js
+++ src/components/Layout/App.js
@@ -6,12 +6,16 @@ import Header from '../Header/Header';
 import ContentContainer from '../../containers/ContentContainer';
 import Footer from '../Footer/Footer';
 
-const App = () => (
-  <div className="app__container">
+const App = () => {
+  navigator.serviceWorker.register("/sw.js");
+  return(
+<div className="app__container">
     <Header className="app__header" />
     <ContentContainer className="app__content" />
     <Footer className="app__footer" />
   </div>
-);
+  )
+
+}
 
 export default App;
